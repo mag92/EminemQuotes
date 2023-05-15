6dfd02cc019ab83e048b05d6e3f5def95c77bdef
Sayori4 <gabrysleszczynski@gmail.com>
2023-05-15 15:32:04 +0200

    Corrected Indexing

--- getquote.php
+++ getquote.php
@@ -3,7 +3,7 @@ $fileContents = file_get_contents("quotes");
 
 $quote = explode("\n", $fileContents);
 
-$randomIndex = random_int(0, sizeof($quote));
+$randomIndex = random_int(0, sizeof($quote) - 1);
 
 if (isset($_GET['format']) && $_GET['format'] == 'json') {
 	header('Content-Type: application/json');
